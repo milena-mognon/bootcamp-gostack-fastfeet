0f5e70747db93390e53eb1c15fed129c19d0563e
milena-mognon <milenamognon@gmail.com>
2020-02-05 11:14:20 -0300

    User Controller and routes added

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -11,6 +12,9 @@ routes.use(authMiddleware);
 /**
  * As rotas abaixo exigem autenticação
  */
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.store);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
